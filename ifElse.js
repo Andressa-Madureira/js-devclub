4feacdded195e39ba17aa1d22605c01814176ca1
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-11-25 15:13:00 -0300

    novo arquivo sobre if & else

---
+++ ifElse.js
@@ -0,0 +1,17 @@
+const notadaAluna = 8
+const notaDeCorte = 5
+
+if(notadaAluna >= notaDeCorte){
+    console.log('Parabéns! Você foi aprovada!')
+}else {
+    console.log('Você foi reprovada!')
+}
+
+const senhaDousuario = 123456
+const senhaDigitada = 1234575
+
+if(senhaDousuario == senhaDigitada){
+    console.log('Seja bem-vindo(a)! Você está logado(a)!')
+}else{
+    console.log('Senha incorreta. Por favor, tente novamente!')
+}
