a1561bd8e95d71a0ca5581beae02a2c37661930c
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-03 14:31:42 -0300

    atualização potência, inclusão de raiz quadrada e cúbica

--- part3/math.js
+++ part3/math.js
@@ -1,3 +1,8 @@
 const resultPow = Math.pow(2,3) //potência
-console.log(resultPow)
+console.log(`O resultado da potência é ${resultPow}`)
 
+const resultRaiz = Math.sqrt(64,2) // raiz quadrada
+const resultCub = Math.sqrt(25,3) // raiz cúbica
+
+console.log(`O resultado da raiz quadrada é ${resultRaiz}`)
+console.log(`O resultado da raiz cúbica é ${resultCub}`)
