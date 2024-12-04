486e92c1e9e3e504ac1d89736ba8911aeee379cd
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-04 15:00:53 -0300

    Operadores de atribuição

---
+++ part3/operadores-atribuicao.js
@@ -0,0 +1,18 @@
+//Operadores de atribuição
+
+let adicaoExemplo = 20 
+adicaoExemplo += 42
+console.log(adicaoExemplo)
+
+let subtracaoExemplo = 20 
+subtracaoExemplo -= 10
+console.log(subtracaoExemplo)
+
+let divisaoExemplo = 20
+divisaoExemplo /= 5
+console.log(divisaoExemplo)
+
+let restoExemplo = 20
+restoExemplo %= 3
+console.log(restoExemplo)
+
