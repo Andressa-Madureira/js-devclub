2f963fd890bad6db3010465e6301e285be9856de
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-04 14:48:20 -0300

    operadores aritméticos: resto, incremento, decremento e exponeciação

---
+++ part3/operadoresaritmeticos.js
@@ -0,0 +1,13 @@
+const res = 8 % 3  //resto -> 8/3 = 2 e restam 2
+console.log(res)
+
+let incr = 20 //incremento (incr + 1)
+incr ++
+console.log(incr)
+
+let decr = 20 //decremento (decr - 1)
+decr --
+console.log(decr)
+
+let resultado = 2 ** 9 //exponenciação
+console.log(resultado)
