23453850eb1c9ab414d3a22ed706633ea2acb73f
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-07 12:29:20 -0300

    Incluindo for

---
+++ part3/for.js
@@ -0,0 +1,9 @@
+for (let i = 5; i <= 15; i ++){
+    console.log(i)
+}
+
+const users = ["Andressa", "Larissa", "Carol", "Marcelle"]
+
+for(let i = 0; i < users.length; i++){
+    console.log(users[i])
+}
